72ffd41ba1b441b6b08680394365716520ec10b4
RaulCruzTorres <raul.cruz@sadesis.com>
2025-05-23 10:01:16 -0400

    Estado de red

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
 import { ElapsedTimeService } from './services/elapsed-time.service';
 import { DeveloperModeService } from './services/developer-mode.service';
+import { Network, NetworkStatus } from '@capacitor/network';
 
 @Component({
   selector: 'app-root',
@@ -38,6 +39,9 @@ export class AppComponent implements OnInit, OnDestroy {
   public elapsedTime: number = 0;
   public developerMode: boolean = false;
 
+  public isConnected: boolean = true; // <-- Variable para guardar el estado de red
+  private networkListener: any; // <-- Guardar la suscripción para removerla luego
+
   constructor(
     private platform: Platform,
     private locationService: LocationService,
@@ -47,14 +51,26 @@ export class AppComponent implements OnInit, OnDestroy {
 
   async ngOnInit() {
     await this.platform.ready();
+
     this.developerMode = await this.developerModeService.isDeveloperMode();
     this.elapsedTime = await this.elapsedTimeService.getElapsedTime();
+
+    const status: NetworkStatus = await Network.getStatus();
+    this.isConnected = status.connected;
+    console.log('[AppComponent] Estado inicial de red:', status);
+
+    this.networkListener = Network.addListener('networkStatusChange', (status: NetworkStatus) => {
+      this.isConnected = status.connected;
+      console.log('[AppComponent] Cambio de red:', status);
+    });
+
     await this.locationService.init();
 
     this.locationSub = this.locationService.locationHistory$.subscribe(history => {
       this.locations = history;
       console.log('[AppComponent] Historial actualizado:', this.locations);
     });
+
   }
 
   async testSave() {
@@ -67,5 +83,6 @@ export class AppComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.locationSub?.unsubscribe();
+    this.networkListener?.remove();
   }
 }
